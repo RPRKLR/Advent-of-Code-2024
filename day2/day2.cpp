0af90aa3f02d0e0ee38a1698b21b62e0552820d7
Péter Dvorák <pdvorak370@gmail.com>
2024-12-02 15:35:02 +0100

    add day2 part2

--- day2/day2.cpp
+++ day2/day2.cpp
@@ -8,6 +8,31 @@
 #include "../utils/utils.hpp"
 using namespace std;
 
+bool getRowTruth(vector<int> input)
+{
+    bool increasing = input[1] > input[0];
+    for (size_t i = 1; i < input.size(); ++i)
+    {
+        int diff = input[i] - input[i - 1];
+
+        if (diff < -3 || diff > 3 || diff == 0)
+        {
+            return false;
+        }
+
+        // Check if the trend is consistent
+        if (increasing && diff < 0)
+        {
+            return false;
+        }
+        if (!increasing && diff > 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main(int argc, char *argv[])
 {
     fstream input("input.txt");
@@ -37,28 +62,30 @@ int main(int argc, char *argv[])
 
     for (size_t i = 0; i < rows.size(); ++i)
     {
-        increasing = rows[i][1] > rows[i][0];
-        for (size_t j = 1; j < rows[i].size(); ++j)
-        {
-            int diff = rows[i][j] - rows[i][j - 1];
-
-            if (diff < -3 || diff > 3 || diff == 0)
-            {
-                break;
-            }
-
-            // Check if the trend is consistent
-            if (increasing && diff < 0)
-            {
-                break;
-            }
-            if (!increasing && diff > 0)
-            {
-                break;
-            }
-            if (j == rows[i].size() - 1)
-                part1_solution_++;
-        }
+        if (getRowTruth(rows[i]))
+            part1_solution_++;
     }
     printSolutionPart1();
+    part2_solution_ = 0;
+    for (size_t i = 0; i < rows.size(); ++i)
+    {
+        if (getRowTruth(rows[i]))
+            part2_solution_++;
+        else
+        {
+            vector<vector<int>> separated_data;
+            vector<int> temp_vec;
+            for (size_t j = 0; j < rows[i].size(); ++j)
+            {
+                temp_vec = rows[i];
+                temp_vec.erase(temp_vec.begin() + j);
+                if (getRowTruth(temp_vec))
+                {
+                    part2_solution_++;
+                    break;
+                }
+            }
+        }
+    }
+    printSolutionPart2();
 }
